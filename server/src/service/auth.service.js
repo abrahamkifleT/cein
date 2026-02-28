5263e70b2b1ac503152bc5f818115bc54d29ca96
abraham <abrahamkifle1212@gmail.com>
2026-02-28 22:52:36 +0300

    feat(auth): implement user registration and login services

---
+++ server/src/service/auth.service.js
@@ -0,0 +1,37 @@
+import User from "../models/user.model.js"
+import bcrypt from "bcryptjs"
+import { generateToken } from "../utils/generateToken.js"
+
+export const registerUserService = async (data) => {
+    const existingUser = await User.findOne({email: data.email});
+    if(existingUser){
+        throw new Error("User already exisist")
+    }
+
+    const hashedPassword = await bcrypt.hash(data.password, 10)
+
+    const user = await User.create({
+        name: data.name,
+        email: data.email,
+        password: data.password,
+    })
+
+    const token = generateToken(user._id);
+    return {user, token}
+}
+
+export const loginUserService = async(data) => {
+    const user = await User.findOne({email: data.email})
+
+    if(!user){
+        throw new Error("Invalid email")
+    }
+
+    const isMatch = await bcrypt.compare(data.password, user.password);
+    if(isMatch){
+        throw Error("Invalid password")
+    }
+
+    const token = generateToken(user._id)
+    return { user, token }
+}

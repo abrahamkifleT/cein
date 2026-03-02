53a5e8717b2ad7e44a7be1902ed8fbf7c79f78b7
abraham <abrahamkifle1212@gmail.com>
2026-03-02 12:07:48 +0300

    feat(user): add role field with enum and default value to User model
    - Added 'role' field with "user" and "admin" options
    - Set default role to "user"

--- server/src/models/user.model.js
+++ server/src/models/user.model.js
@@ -1,4 +1,4 @@
-import mongoose from "mongoose"
+import mongoose from "mongoose";
 
 const userSchema = new mongoose.Schema({
     name: {
@@ -13,8 +13,13 @@ const userSchema = new mongoose.Schema({
     password: {
         type: String,
         required: true
+    },
+    role: {
+        type: String,
+        enum: ["user", "admin"],
+        default: "user"
     }
-})
+}, { timestamps: true });
 
 const User = mongoose.models.User || mongoose.model("User", userSchema);
 

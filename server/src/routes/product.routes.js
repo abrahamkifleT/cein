bbf70038e87e3558e1373a9cd7242aef63fb856c
abraham <abrahamkifle1212@gmail.com>
2026-03-02 21:15:49 +0300

    feat(routes): add image upload middleware to create product route
    - Imported and applied the `upload` middleware to the `/create` endpoint for products
    - Configured to accept an array of up to 5 images
    - Formatted middleware spacing in `update-product` and `delete-product` routes

--- server/src/routes/product.routes.js
+++ server/src/routes/product.routes.js
@@ -1,13 +1,14 @@
 import express from "express"
 import { createProduct, getProducts, getProductById, updateProduct, deleteProduct } from "../controller/product.controller.js"
 import { protect, admin } from "../middleware/auth.middleware.js"
+import { upload } from "../middleware/upload.middleware.js"
 
 const router = express.Router();
 
-router.post("/create",protect, admin, createProduct)
+router.post("/create", protect, admin, upload.array("images", 5), createProduct)
 router.get("/get-products", getProducts)
 router.get("/get-product/:id", getProductById)
-router.put("/update-product/:id",protect, admin, updateProduct)
-router.delete("/delete-product/:id",protect, admin, deleteProduct)
+router.put("/update-product/:id", protect, admin, updateProduct)
+router.delete("/delete-product/:id", protect, admin, deleteProduct)
 
 export default router

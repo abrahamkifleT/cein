5e313a74d2d0b4bbc69ce8bdac89de27e0c84e8d
abraham <abrahamkifle1212@gmail.com>
2026-03-04 16:34:27 +0300

    feat(frontend): implement CollectionContext for state management

---
+++ frontend/src/context/collection.context.jsx
@@ -0,0 +1,34 @@
+import { createContext, useContext, useEffect, useState } from "react";
+import { getAllCollections } from "../services/collection.service";
+
+const CollectionContext = createContext();
+
+export const CollectionContextProvider = ({ children }) => {
+    const [collections, setCollections] = useState([]);
+    const [loading, setLoading] = useState(true);
+    const [error, setError] = useState(null);
+
+    useEffect(() => {
+        const fetchCollections = async () => {
+            try {
+                const data = await getAllCollections();
+                setCollections(data);
+            } catch (error) {
+                setError(error);
+            } finally {
+                setLoading(false);
+            }
+        };
+        fetchCollections();
+    }, []);
+
+    return (
+        <CollectionContext.Provider value={{ collections, loading, error }}>
+            {children}
+        </CollectionContext.Provider>
+    );
+};
+
+export const useCollections = () => {
+    return useContext(CollectionContext);
+};

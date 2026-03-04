eb3dd56325b71071d5dbc9a1c58a52da682df061
abraham <abrahamkifle1212@gmail.com>
2026-03-04 15:36:01 +0300

    feat(frontend): create initial routing with Home component

---
+++ frontend/src/app/routes.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import Home from '../pages/Home/Home';
+
+const AppRoutes = () => {
+    return (
+        <>
+            <Home />
+            {/* Add more routes here when react-router-dom is installed */}
+        </>
+    );
+};
+
+export default AppRoutes;

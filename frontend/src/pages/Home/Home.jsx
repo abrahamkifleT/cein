cba5ed9f13c3e5b4c0c1f8fe59ac060d9b0f3842
abraham <abrahamkifle1212@gmail.com>
2026-03-04 16:35:17 +0300

    feat(home): add ShopByCollection component to Home page

--- frontend/src/pages/Home/Home.jsx
+++ frontend/src/pages/Home/Home.jsx
@@ -1,12 +1,14 @@
 import React from 'react';
 import Hero from '../../components/home/Hero/Hero';
 import ShopByCategory from '../../components/home/ShopByCategory/ShopByCategory';
+import ShopByCollection from '../../components/home/ShopByCollection/ShopByCollection';
 
 const Home = () => {
     return (
         <div className="home-page">
             <Hero />
             <ShopByCategory />
+            <ShopByCollection />
         </div>
     );
 };

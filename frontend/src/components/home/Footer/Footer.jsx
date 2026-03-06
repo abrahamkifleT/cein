eee404d1d533ec540aa185c94396e3f154a3aeb5
abraham <abrahamkifle1212@gmail.com>
2026-03-06 16:12:06 +0300

    feat: add Footer component with navigation links
    - Create Footer component with Account, Company, Get Help, and Connect sections
    - Include email input field placeholder for newsletter
    - Add empty Footer.css placeholder file

---
+++ frontend/src/components/home/Footer/Footer.jsx
@@ -0,0 +1,56 @@
+import React from 'react'
+
+const Footer = () => {
+    return (
+        <div>
+            <div>
+                <div>
+                    <h2>Account</h2>
+                    <p>Log in</p>
+                    <p>Sign up</p>
+                    <p>Redeem a Gift Card</p>
+                </div>
+
+                <div>
+                    <h2>Company</h2>
+                    <p>About</p>
+                    <p>Environmental initiatives</p>
+                    <p>Factories</p>
+                    <p>DEI</p>
+                    <p>Careers</p>
+                    <p>International</p>
+                    <p>Accessibility</p>
+                </div>
+
+                <div>
+                    <h2>Get Help</h2>
+                    <p>Help Center</p>
+                    <p>Return Policy</p>
+                    <p>Shipping Info</p>
+                    <p>Bulk Orders</p>
+                </div>
+
+                <div>
+                    <h2>Connect</h2>
+                    <p>Facebook</p>
+                    <p>Instagram</p>
+                    <p>Twitter</p>
+                    <p>Affiliates</p>
+                    <p>Out Stores</p>
+                </div>
+
+                <div>
+                    <input type="text" />
+                    {/* arrow Icon */}
+                </div>
+            </div>
+ 
+        <div>
+            
+        </div>
+
+        </div>
+    )
+}
+
+export default Footer

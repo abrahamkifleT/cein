d37b4f10e9c7397683bae670c9c874103b2a3ecd
abraham <abrahamkifle1212@gmail.com>
2026-03-07 16:55:40 +0300

    feat(About): add mission section and implement responsive styling
    - Added the "about-mission" section to About.jsx detailed the Radical Transparency philosophy.
    - Implemented responsive fluid typography in About.css using the clamp() function for headers and body text.
    - Enhanced About Hero to use a responsive height (clamp(400px, 80vh, 691px)).
    - Styled the new mission section with a centered layout and optimized vertical spacing (min-height 525px).

--- frontend/src/components/About/About.jsx
+++ frontend/src/components/About/About.jsx
@@ -4,13 +4,21 @@ import heroImage from '../../assets/images/about/about_page_hero.png';
 
 const AboutHero = () => {
     return (
-        <div className="about-hero">
-            <img src={heroImage} alt="About Hero" className="about-hero__image" />
-            <div className="about-hero__content">
-                <h1>We believe we can all make difference.</h1>
-                <p>Our way: Exceptional quality Ethical factories. Radical Transparency.</p>
+        <>
+            <div className="about-hero">
+                <img src={heroImage} alt="About Hero" className="about-hero__image" />
+                <div className="about-hero__content">
+                    <h1>We believe we can all make difference.</h1>
+                    <p>Our way: Exceptional quality Ethical factories. Radical Transparency.</p>
+                </div>
             </div>
-        </div>
+
+            <div className='about-mission'>
+                <div className='about-mission__container'>
+                    <h4>At Everlane, we want the right choice to be as easy as putting on a great T-shirt. That’s why we partner with the best, ethical factories around the world. Source only the finest materials. And share those stories with you—down to the true cost of every product we make. It’s a new way of doing things. We call it Radical Transparency.</h4>
+                </div>
+            </div>
+        </>
     )
 }
 

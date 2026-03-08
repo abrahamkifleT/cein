cff3742d2f7093650e341f7735c0db0f58e11781
abraham <abrahamkifle1212@gmail.com>
2026-03-08 19:59:34 +0300

    feat(about): assemble modular sections on About page
    - Updated AboutHero import to the new modular directory.
    - Integrated AboutMission, FeatureSection, ComparisonSection, and ExploreMore components.
    - Structured the About page as a sequence of focused layout sections within the main tag.

--- frontend/src/pages/About/About.jsx
+++ frontend/src/pages/About/About.jsx
@@ -1,10 +1,18 @@
 import React from 'react'
-import AboutHero from '../../components/About/About'
+import AboutHero from '../../components/About/AboutHero/AboutHero'
+import AboutMission from '../../components/About/AboutMission/AboutMission'
+import FeatureSection from '../../components/About/FeatureSection/FeatureSection'
+import ComparisonSection from '../../components/About/ComparisonSection/ComparisonSection'
+import ExploreMore from '../../components/About/ExploreMore/ExploreMore'
 
 const About = () => {
     return (
         <main>
             <AboutHero />
+            <AboutMission />
+            <FeatureSection />
+            <ComparisonSection />
+            <ExploreMore />
         </main>
     )
 }
